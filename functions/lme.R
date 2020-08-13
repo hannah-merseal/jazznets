7d06f0dcd174cd0def2e62a254d3bd8ba7d0e7b0
hannah-merseal <hmm5708@psu.edu>
2020-08-13 14:41:18 -0400

    more detailed notes pre lme

--- functions/lme.R
+++ functions/lme.R
@@ -9,29 +9,52 @@ library(lme4)
 JNmaster <- read.csv("pilot data/master_info_excludes/master.csv")
 surveyAll <- read.csv("pilot data/JNPilotSurvey.csv")
 
+#pull relevant survey information for analysis, rename for consistency
 survey <- surveyAll %>% dplyr::select(participant, musicianYN, EmotionalContent, Contour, Dissonance, 
                                       IntervalVariety, IntervalSize, ImpliedHarmony, HoursWeekListen, 
                                       HoursWeekListenJazz, InstrumentYN, YearsFormalTraining, 
                                       PrimaryProficiency, YearsFormalImp, ImpProficiency, 
-                                      YearsTheory, PlayHoursNow, ImpHoursNow, PercentImp)
+                                      YearsTheory, PlayHoursNow, ImpHoursNow, PercentImp) %>%
+  rename(emotionalContent = EmotionalContent,
+         contour = Contour,
+         dissonance = Dissonance,
+         intervalVariety = IntervalVariety,
+         intervalSize = IntervalSize,
+         impliedHarmony = ImpliedHarmony,
+         hoursWeekListen = HoursWeekListen,
+         hoursWeekListenJazz = HoursWeekListenJazz,
+         instrumentYN = InstrumentYN,
+         yearsFormalTraining = YearsFormalTraining,
+         primaryProficiency = PrimaryProficiency,
+         yearsFormalImp = YearsFormalImp,
+         impProficiency = ImpProficiency,
+         yearsTheory = YearsTheory,
+         playHoursNow = PlayHoursNow,
+         impHoursNow = ImpHoursNow,
+         percentImp = PercentImp)
+#MERGE
+master <- merge(JNmaster, survey)
 
+# do musicians and non-musicians use different strategies? 
+# w/ emotionalContent, contour, dissonance, intervalVariety, intervalSize, impliedHarmony
 
-#up through distance 4 these overlap
+#lme covariates for response:
+# overall listening hours
+# jazz listening hours
+# instrument
+# instrument proficiency
+# improvisation proficiency
+# playing hours
+# imp hours
+# percent imp
+
+#then do it again for RT
+
+#up through distance 4 the stimuli overlap
 #between 5 and 6 no overlap - there is a difference!
-#add standard error to ggplots
-#just trials that are related
+#for 1, 2, 3, 4 - remove trials with response = 0
+#for 6, 10 - remove trials with response = 1
+#THEN look at RT
 
-#just trials that are unrelated - 5 and up (coincides with no overlap)
-
-#classical v jazz v non
-
-#listening hours as covariate
-
-#music expertise as covariate
-
-#lme4 package!
-
-
-
-#what distances are definitely related?
-#what happens after distance = 10?
+#ggplot for response (don't forget error bars)
+#ggplot for RT (just correct trials)
