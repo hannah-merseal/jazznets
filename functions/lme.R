b14a3c5c82d5aeca120fe671abdef7818eff3376
hannah-merseal <hmm5708@psu.edu>
2020-11-29 17:51:47 -0500

    series of logistic regressions for response at each split

--- functions/lme.R
+++ functions/lme.R
@@ -1,3 +1,5 @@
+##### LIBRARIES & CREATE MASTER #####
+# pull libraries and create master dataframe
 library(tidyverse)
 library(psych)
 library(car)
@@ -39,7 +41,7 @@ survey <- surveyAll %>% dplyr::select(prolificID, musicianYN, EmotionalContent, 
 #MERGE
 master <- merge(JNmaster, survey)
 
-# predictors for response:
+# music predictors:
 # overall listening hours - hoursWeekListen W
 # jazz listening hours - hoursWeekListenJazz W
 # primary instrument proficiency - primaryProficiency normal W*
@@ -47,14 +49,16 @@ master <- merge(JNmaster, survey)
 # playing hours - playHoursNow W
 # improv hours - impHoursNow W*
 # percent improv played - percentImp W
-#lmm.basic <- lmer(response ~ distance*musicianYN + impHoursNow + (1 | participant), data = master)
-#summary(lmm.basic)
 
-#RESPONSE
-#logistic regression with chisq
+
+
+##### RESPONSE #####
+
+### RESPONSE WITH 20 ###
+#logistic regression with chisq WITH 20
 log.modelC <- glm(response ~ 1, data = master, family = "binomial")
 summary(log.modelC)
-log.modelA <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz + playHoursNow + impHoursNow + percentImp, data = master, family = "binomial")
+log.modelA <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz, data = master, family = "binomial")
 summary(log.modelA)
 anova(log.modelC, log.modelA, test = "Chisq")
 
@@ -66,17 +70,123 @@ logtable <-  data.frame(observed = master$response,
                         predicted = ifelse(fitted(log.modelA) > .5, 1, 0))
 xtabs(~ observed + predicted, data = logtable)
 
-#musician interaction + musician variables
-musicians <- master %>% dplyr::filter(musicianYN == 1)
-musician.int <- glm(response ~ distance, data = musicians, family = "binomial")
+
+
+### RESPONSE WITHOUT 20 ###
+#logistic regression with chisq WITHOUT 20
+masterNo20 <- master %>% dplyr::filter(distance != 20)
+log.modelC <- glm(response ~ 1, data = masterNo20, family = "binomial")
+summary(log.modelC)
+log.modelA <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz, data = masterNo20, family = "binomial")
+summary(log.modelA)
+anova(log.modelC, log.modelA, test = "Chisq")
+
+#converting log odds into odds ratio
+exp(cbind(OR = coef(log.modelA), confint(log.modelA)))
+
+#confusion table
+logtable <-  data.frame(observed = master$response,
+                        predicted = ifelse(fitted(log.modelA) > .5, 1, 0))
+xtabs(~ observed + predicted, data = logtable)
+
+
+
+### RESPONSE JUST 10 & 20 ###
+master1020 <- master %>% dplyr::filter(distance %in% c(10, 20))
+
+#logistic regression with chisq JUST 10 & 20
+log.modelC <- glm(response ~ 1, data = master1020, family = "binomial")
+summary(log.modelC)
+log.modelA <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz, data = master1020, family = "binomial")
+summary(log.modelA)
+anova(log.modelC, log.modelA, test = "Chisq")
+
+#converting log odds into odds ratio
+exp(cbind(OR = coef(log.modelA), confint(log.modelA)))
+
+#confusion table
+logtable <-  data.frame(observed = master$response,
+                        predicted = ifelse(fitted(log.modelA) > .5, 1, 0))
+xtabs(~ observed + predicted, data = logtable)
+
+
+
+### RESPONSE WITHOUT 20 MUSICIANS/NON ###
+#musician interaction (which is NS) + musician variables
+musicians <- masterNo20 %>% dplyr::filter(musicianYN == 1)
+musician.int <- glm(response ~ distance + hoursWeekListen + hoursWeekListenJazz + 
+                      primaryProficiency + impProficiency +
+                      playHoursNow + impHoursNow + percentImp, data = musicians, family = "binomial")
 summary(musician.int)
 exp(cbind(OR = coef(musician.int), confint(musician.int)))
 
-#nonmusician interaction
-nonmusicians <- master %>% dplyr::filter(musicianYN == 0)
+#nonmusician interaction (NS)
+nonmusicians <- masterNo20 %>% dplyr::filter(musicianYN == 0)
 non.int <- glm(response ~ distance, data = nonmusicians, family = "binomial")
 summary(non.int)
 exp(cbind(OR = coef(non.int), confint(non.int)))
+
+
+
+### RESPONSE 10-20 MUSICIANS ###
+musicians <- master1020 %>% dplyr::filter(musicianYN == 1)
+musician.int <- glm(response ~ distance + hoursWeekListen + hoursWeekListenJazz + 
+                      primaryProficiency + impProficiency +
+                      playHoursNow + impHoursNow + percentImp, data = musicians, family = "binomial")
+summary(musician.int)
+exp(cbind(OR = coef(musician.int), confint(musician.int)))
+
+#nonmusician interaction (NS)
+nonmusicians <- master1020 %>% dplyr::filter(musicianYN == 0)
+non.int <- glm(response ~ distance, data = nonmusicians, family = "binomial")
+summary(non.int)
+exp(cbind(OR = coef(non.int), confint(non.int)))
+
+##### RESPONSE PLOTS #####
+
+#ggplot for response (+ error)
+respPlot <- ggplot(master,
+                   aes(x = as.factor(distance),
+                       y = response,
+                       group = musicianYN,
+                       color = as.factor(musicianYN))) +
+  stat_summary(fun.y = "mean", geom = "point") +
+  stat_summary(fun.y = "mean", geom = "line") +
+  stat_summary(fun.data = "mean_se", geom = "errorbar") +
+  labs(x = "Distance",
+       y = "Mean Response",
+       title = "Mean Response for Distance (With 20), Musicians vs. Nons") +
+  scale_color_manual(labels = c("Nonmusicians", "Musicians"), values = c("blue", "red")) +
+  guides(color = guide_legend("Group"))+
+  theme_bw()
+respPlot
+
+#response but without 20
+master10 <- master %>% dplyr::filter(distance != 20)
+
+log.modelC10 <- glm(response ~ 1, data = master10, family = "binomial")
+summary(log.modelC10)
+log.modelA10 <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz, data = master10, family = "binomial")
+summary(log.modelA10)
+anova(log.modelC10, log.modelA10, test = "Chisq")
+
+respPlot10 <- ggplot(master10,
+                     aes(x = as.factor(distance),
+                         y = response,
+                         group = musicianYN,
+                         color = as.factor(musicianYN))) +
+  stat_summary(fun.y = "mean", geom = "point") +
+  stat_summary(fun.y = "mean", geom = "line") +
+  stat_summary(fun.data = "mean_se", geom = "errorbar") +
+  labs(x = "Distance",
+       y = "Mean Response",
+       title = "Mean Response for Distance (No 20), Musicians vs. Nons") +
+  scale_color_manual(labels = c("Nonmusicians", "Musicians"), values = c("blue", "red")) +
+  guides(color = guide_legend("Group"))+
+  theme_bw()
+respPlot10
+
+##### RT #####
 
 #regression for RT with only correct trials
 masterCorr <- read.csv("data/master_info_excludes/correctTrials.csv")
@@ -99,48 +209,7 @@ nonRT <- masterCorr %>% dplyr::filter(musicianYN == 0)
 nonRT.lm <- lm(RT ~ distance, data = nonRT)
 mcSummary(nonRT.lm)
 
-#ggplot for response (+ error)
-respPlot <- ggplot(master,
-                   aes(x = as.factor(distance),
-                       y = response,
-                       group = musicianYN,
-                       color = as.factor(musicianYN))) +
-  stat_summary(fun.y = "mean", geom = "point") +
-  stat_summary(fun.y = "mean", geom = "line") +
-  stat_summary(fun.data = "mean_se", geom = "errorbar") +
-  labs(x = "Distance",
-       y = "Mean Response",
-       title = "Mean Response for Distance (With 20), Musicians vs. Nons") +
-  scale_color_manual(labels = c("Nonmusicians", "Musicians"), values = c("blue", "red")) +
-  guides(color = guide_legend("Group"))+
-  theme_bw()
-respPlot
-
-#response but without 20
-master10 <- master %>% dplyr::filter(distance != 20)
-  
-log.modelC10 <- glm(response ~ 1, data = master10, family = "binomial")
-summary(log.modelC10)
-log.modelA10 <- glm(response ~ distance*musicianYN + hoursWeekListen + hoursWeekListenJazz, data = master10, family = "binomial")
-summary(log.modelA10)
-anova(log.modelC10, log.modelA10, test = "Chisq")
-
-respPlot10 <- ggplot(master10,
-                   aes(x = as.factor(distance),
-                       y = response,
-                       group = musicianYN,
-                       color = as.factor(musicianYN))) +
-  stat_summary(fun.y = "mean", geom = "point") +
-  stat_summary(fun.y = "mean", geom = "line") +
-  stat_summary(fun.data = "mean_se", geom = "errorbar") +
-  labs(x = "Distance",
-       y = "Mean Response",
-       title = "Mean Response for Distance (No 20), Musicians vs. Nons") +
-  scale_color_manual(labels = c("Nonmusicians", "Musicians"), values = c("blue", "red")) +
-  guides(color = guide_legend("Group"))+
-  theme_bw()
-respPlot10
-
+##### RT PLOTS #####
 #ggplot for RT (just correct trials)
 library(effects)
 predicted.values <- effect('distance*musicianYN', model1.RT,
